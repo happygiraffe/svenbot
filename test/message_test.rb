93693fcf5ee8d2005ef966ef649313bf3695e2d6
Dominic Mitchell <dom@happygiraffe.net>
2008-12-17 00:44:49 +0000

    Add a simple html message helper.

---
+++ test/message_test.rb
@@ -0,0 +1,17 @@
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'message'
+
+class MessageTest < Test::Unit::TestCase
+  include Message
+
+  def test_html_message
+    msg = html_message("hello <em>world</em>")
+    assert_equal :chat, msg.type
+    assert_equal "hello world", msg.body
+    assert_equal "<html xmlns='http://jabber.org/protocol/xhtml-im'><body " + 
+      "xmlns='http://www.w3.org/1999/xhtml'>hello <em>world</em></body></html>",
+      msg.first_element('html').to_s
+  end
+end
