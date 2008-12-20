6e60e1065c27e7607a0cc3ac04c8cadbd7fea2ac
Dominic Mitchell <dom@happygiraffe.net>
2008-12-20 22:54:58 +0000

    Rename bot field to "um".

--- test/user_manager_test.rb
+++ test/user_manager_test.rb
@@ -5,79 +5,79 @@ require 'commit'
 require 'user_manager'
 
 class UserManagerTest < Test::Unit::TestCase
-  attr_reader :bot
+  attr_reader :um
   # Sample data
   A_JID = 'me@example.com'
   # More sample data
   ANOTHER_JID = 'you@example.com'
 
   def setup
-    @bot = UserManager.new
+    @um = UserManager.new
   end
 
   def test_register_user
-    bot.register A_JID, '/'
+    um.register A_JID, '/'
 
-    assert_equal 1, bot.users.size
-    assert_equal ['/'], bot.users[A_JID].paths
+    assert_equal 1, um.users.size
+    assert_equal ['/'], um.users[A_JID].paths
 
-    assert_equal 1, bot.paths.size
-    assert_equal [A_JID], bot.paths['/'].collect { |u| u.jid }
+    assert_equal 1, um.paths.size
+    assert_equal [A_JID], um.paths['/'].collect { |u| u.jid }
   end
 
   def test_register_two_users_same_path
-    bot.register A_JID, '/proj'
-    bot.register ANOTHER_JID, '/proj'
+    um.register A_JID, '/proj'
+    um.register ANOTHER_JID, '/proj'
 
-    assert_equal 2, bot.users.size
-    assert_equal ['/proj'], bot.users[A_JID].paths
-    assert_equal ['/proj'], bot.users[ANOTHER_JID].paths
+    assert_equal 2, um.users.size
+    assert_equal ['/proj'], um.users[A_JID].paths
+    assert_equal ['/proj'], um.users[ANOTHER_JID].paths
 
-    assert_equal 1, bot.paths.size
-    assert_equal [A_JID, ANOTHER_JID], jids_for_path(bot, '/proj').sort
+    assert_equal 1, um.paths.size
+    assert_equal [A_JID, ANOTHER_JID], jids_for_path(um, '/proj').sort
   end
 
   def test_register_two_paths
-    bot.register A_JID, '/proj1'
-    bot.register A_JID, '/proj2'
+    um.register A_JID, '/proj1'
+    um.register A_JID, '/proj2'
 
-    assert_equal 1, bot.users.size
-    assert_equal ['/proj1', '/proj2'], bot.users[A_JID].paths
+    assert_equal 1, um.users.size
+    assert_equal ['/proj1', '/proj2'], um.users[A_JID].paths
 
-    assert_equal 2, bot.paths.size
-    assert_equal [A_JID], jids_for_path(bot, '/proj1')
-    assert_equal [A_JID], jids_for_path(bot, '/proj2')
+    assert_equal 2, um.paths.size
+    assert_equal [A_JID], jids_for_path(um, '/proj1')
+    assert_equal [A_JID], jids_for_path(um, '/proj2')
   end
 
   def test_unregister
-    bot.register A_JID, '/proj1'
-    bot.unregister A_JID, '/proj1'
+    um.register A_JID, '/proj1'
+    um.unregister A_JID, '/proj1'
 
-    assert_equal 0, bot.users.size
-    assert_equal 0, bot.paths.size
+    assert_equal 0, um.users.size
+    assert_equal 0, um.paths.size
   end
 
   def test_commit_messages
-    bot.register(A_JID, '/proj1')
+    um.register(A_JID, '/proj1')
     c = Commit.new('12345', 'arthur', '/proj1', 'fix bug 42')
-    msgs = bot.commit_messages c
+    msgs = um.commit_messages c
     assert_equal 1, msgs.size
     assert_equal A_JID, msgs[0].to.to_s
     assert_equal 'arthur committed 12345: fix bug 42', msgs[0].body
   end
 
   def test_commit_messages_understand_prefix
-    bot.register(A_JID, '/')
+    um.register(A_JID, '/')
     c = Commit.new('12345', 'arthur', '/proj1/README', 'fix bug 42')
-    msgs = bot.commit_messages c
+    msgs = um.commit_messages c
     assert_equal 1, msgs.size
     assert_equal A_JID, msgs[0].to.to_s
   end
 
   def test_paths_for
-    bot.register A_JID, '/proj1'
-    bot.register A_JID, '/proj2'
-    assert_equal %w[ /proj1 /proj2 ], bot.paths_for(A_JID).sort
+    um.register A_JID, '/proj1'
+    um.register A_JID, '/proj2'
+    assert_equal %w[ /proj1 /proj2 ], um.paths_for(A_JID).sort
   end
 
   private
