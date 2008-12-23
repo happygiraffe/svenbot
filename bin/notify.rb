3b514a96cd1cd0f20c6d56b9457b93e46b3994d5
Dominic Mitchell <dom@happygiraffe.net>
2008-12-23 00:54:50 +0000

    Make notify follow svenbot conventions.
    And look for the socket in the correct place.

--- bin/notify.rb
+++ bin/notify.rb
@@ -1,9 +1,11 @@
 # Send a message to the bot.
 
 require 'socket'
+require 'svenbot/repo'
 
-repo, rev = ARGV
+repo = Svenbot::Repo.new(ARGV[0])
+rev = ARGV[1]
 
-sock = UNIXSocket.open "#{repo}/sock"
+sock = UNIXSocket.open "#{repo.meta_dir}/sock"
 sock.send rev, 0
 sock.close
